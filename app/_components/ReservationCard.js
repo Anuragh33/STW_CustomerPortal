77effe320f8f02e1e256177238053469a47c4593
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-09-09 20:35:06 -0500

    Update ReservationCard.js

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -71,7 +71,7 @@ function ReservationCard({ booking, onDelete }) {
       </div>
 
       <div className="flex flex-col border-l border-primary-800 w-[100px]">
-        {!isPast(new Date(startDate)) ? (
+        {!isPast(startDate) ? (
           <>
             {' '}
             <Link
